b4a5f5931f94f20b7d3c5be1fdacc13ddcfabf4e
Jonas jost <jonas.jost@accenture.com>
2022-02-08 17:54:38 +0100

    Stupid not defined changes

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -2,58 +2,50 @@ package io.jzheaux.springsecurity.resolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.sql.DataSource;
-import java.util.List;
-
 import static org.springframework.http.HttpMethod.GET;
+
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@SpringBootApplication()
+@SpringBootApplication
 public class ResolutionsApplication extends WebSecurityConfigurerAdapter {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ResolutionsApplication.class, args);
-	}
-
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 				.authorizeRequests(authz -> authz
-						.anyRequest().authenticated())
+						.mvcMatchers(GET, "/resolutions", "/resolution/**").hasAuthority("resolution:read")
+						.anyRequest().hasAuthority("resolution:write"))
 				.httpBasic(basic -> {})
-				.cors(cors->{});
+				.cors(cors -> {});
 	}
 
 	@Bean
-	UserDetailsService userDetailsService(UserRepository users) {
+	public UserDetailsService userDetailsService(UserRepository users) {
 		return new UserRepositoryUserDetailsService(users);
 	}
 
 	@Bean
-	WebMvcConfigurer webMvcConfigurer() {
+	public WebMvcConfigurer webMvcConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-//						 .maxAge(0) // if using local verification
 						.allowedOrigins("http://localhost:4000")
 						.allowedMethods("HEAD")
 						.allowedHeaders("Authorization");
 			}
 		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResolutionsApplication.class, args);
 	}
 
 }
